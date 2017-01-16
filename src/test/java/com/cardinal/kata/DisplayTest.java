207d9a498380b2893fde413bec39aaaaf03aa9d4
Lisa Jenkins <jenkinsl8@gmail.com>
2017-01-16 23:54:13 +0000

    added additional test methods and comment

--- src/test/java/com/cardinal/kata/DisplayTest.java
+++ src/test/java/com/cardinal/kata/DisplayTest.java
@@ -8,20 +8,30 @@ import org.junit.Test;
  * Test the vendingMachine display logic
  */
 public class DisplayTest {
-    VendingMachineDisplay display;
+    VendingMachineDisplay vendingMachineDisplay;
    
     
     @Before
     public void init() {
-        display = new VendingMachineDisplay();
+        vendingMachineDisplay = new VendingMachineDisplay();
     }
     
     /**
      * When there are no coins inserted, the machine displays INSERT COIN.
+     * Validates that the default value of the display is "INSERT COIN".
      */
     @Test
-    public void newCoinInsertedTest() {
-        assertEquals("INSERT COIN", display.getDisplayText());
+    public void noCoinInsertedTest() {
+        assertEquals("INSERT COIN", vendingMachineDisplay.getDisplayText());
+    }
+    
+    /**
+     * validates that the display text can be set.
+     */
+    @Test
+    public void changeDisplayText() {
+        vendingMachineDisplay.setDisplayText("1.00");
+        assertEquals("1.00", vendingMachineDisplay.getDisplayText());
     }
     
 }
